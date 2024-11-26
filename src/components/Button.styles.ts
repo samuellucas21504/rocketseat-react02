8a668b515a3cd009065a169ac29dc800b975464e
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2024-11-26 08:04:49 -0300

    First commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${props => props.theme.primary}
+`
+
+//   ${
+//   props => {
+//     return `background-color: ${buttonVariants[props.variant]}`
+//   }
+// }
+
